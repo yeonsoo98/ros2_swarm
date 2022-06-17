b7e345b57cddf7d65e7ac83b712deed1566826b4
yeonsoo98 <yeonso981@naver.com>
2022-06-17 17:53:29 +0900

    burger and number change

--- start_robot.sh
+++ start_robot.sh
@@ -17,8 +17,8 @@ colcon build --symlink-install &&
  ROS_DOMAIN_ID=42 ros2 launch ros2swarm bringup_robot.launch.py \
  pattern:=dispersion_pattern \
  log_level:=info \
- robot:=waffle_pi \
- robot_number:=NUM_CHANGE_ME
+ robot:=burger \
+ robot_number:=2
 # pattern_name:
 ## movement pattern: drive_pattern | dispersion_pattern | aggregation_pattern | flocking_pattern | flocking_pattern2 | attraction_pattern | attraction_pattern2 | magnetometer_pattern | minimalist_flocking_pattern | discussed_dispersion_pattern
 ## voting_pattern voter_model_pattern | voter_model_with_limiter_pattern | majority_rule_pattern
