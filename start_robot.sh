5f9344670d0b00d4aca77fbbb5cbb068f2ca6f95
yeonsoo98 <yeonso981@naver.com>
2022-07-04 09:56:53 +0900

    change robot

--- start_robot.sh
+++ start_robot.sh
@@ -17,7 +17,7 @@ colcon build --symlink-install &&
  ROS_DOMAIN_ID=42 ros2 launch ros2swarm bringup_robot.launch.py \
  pattern:=dispersion_pattern \
  log_level:=info \
- robot:=waffle_pi \
+ robot:=burger \
  robot_number:=2
 # pattern_name:
 ## movement pattern: drive_pattern | dispersion_pattern | aggregation_pattern | flocking_pattern | flocking_pattern2 | attraction_pattern | attraction_pattern2 | magnetometer_pattern | minimalist_flocking_pattern | discussed_dispersion_pattern
